df607f172ac104c7234b599e455366cda696f1c9
cheol <cheoloffice4854@gmail.com>
2024-08-01 16:03:47 +0900

    .

---
+++ src/main/java/EssentialGrade.java
@@ -0,0 +1,27 @@
+public class EssentialGrade {
+    public int EssentialGrade(int Score){
+        if ((Score >= 95) && (Score <= 100)){
+            Grade = A;
+            return Grade;
+        }
+        else if ((Score >= 90) && (Score <= 94)){
+            Grade = B;
+            return Grade;
+        }
+        else if ((Score >= 80) && (Score <= 89)){
+            Grade = C;
+            return Grade;
+        }
+        else if ((Score >= 70) && (Score <= 79)){
+            Grade = D;
+            return Grade;
+        }
+        else if ((Score >= 60) && (Score <= 69)){
+            Grade = F
+            return Grade;
+        }
+        else {
+            Grade = N;
+            return Grade;
+    }
+}
