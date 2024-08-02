3d78a8e9a00028aa777f086cd3c389ea0d373045
taeyoung kang <kty1467@naver.com>
2024-08-02 17:52:37 +0900

    add : 특정 학생 정보 출력 (완)

--- src/main/java/view/StudentView.java
+++ src/main/java/view/StudentView.java
@@ -1,19 +1,19 @@
 package view;
 import data.Data;
-
 import java.util.*;
-import data.Data;
-import module.CreateStudent;
+import module.*;
 
 public class StudentView implements View{
 
     private Data data;
     private CreateStudent createStudent;
+    private StudentInfo studentInfo;
     Scanner sc = new Scanner(System.in);
 
     public StudentView(Data data) {
         this.data = data;
         this.createStudent = new CreateStudent(data);
+        this.studentInfo = new StudentInfo(data);
     }
     @Override
     public void displayView() {
@@ -38,7 +38,7 @@ public class StudentView implements View{
                     createStudent.studentSelect();
                 }
                 case 2 -> {} // 수강생 목록 조회
-                case 3 -> {} // 수강생 정보 조회
+                case 3 -> studentInfo.showInfo(); // 수강생 정보 조회
                 case 4 -> {} // 수강생 정보 수정
                 case 5 -> {} // 수강생 삭제
                 case 6 -> {} // 상태별 수강생 목록 조회
