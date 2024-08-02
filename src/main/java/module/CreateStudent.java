34ac4905eb87f23ec1c060590202dcb8aebc4a55
Hyemin Yang <asitwas729@gmail.com>
2024-08-02 11:46:21 +0900

    주석으로 기능 구현 틀 짰음

--- src/main/java/module/CreateStudent.java
+++ src/main/java/module/CreateStudent.java
@@ -8,16 +8,18 @@ import java.util.Scanner;
 
 public class CreateStudent {
   Data data = new Data();
+  private List<Subject> selectSubjects;
   Scanner sc = new Scanner(System.in);
 
 
   public CreateStudent(){}
 
   public CreateStudent(Data data){
+    // 데이터 초기화 필요
     this.data = data;
-    //데이터 초기화
-    data.setInitData();
   }
+
+
 
   public void addStudent(){
     System.out.print("이름 : ");
@@ -25,7 +27,14 @@ public class CreateStudent {
     data.printSubjects();
     for(int j=0; j<9; j++){
       if(j<5){
+        // List에 넣기
+        // 필수과목인지 선택과목인지 확인
+        // count++(타입별 선택된 수)
 
+      } else if (j >= 5){
+        // 최소 필수과목 3개 이상 충족하는지
+        // 최소 선택과목 2개 이상 충족하는지
+        // 다시 넣기
       }
     }
   }
@@ -40,7 +49,7 @@ public class CreateStudent {
           + subject.getSubjectName() +
           "(" + subject.getSubjectType() + ")");
     }
-    System.out.println("==================================");
+    System.out.println("\n==================================");
   }
 
 }
