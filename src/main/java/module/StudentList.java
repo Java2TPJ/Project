1976c014bc8c6fad00475dff3de71334484d5f45
Hyemin Yang <asitwas729@gmail.com>
2024-08-02 17:55:29 +0900

    StudentList.java 생성

---
+++ src/main/java/module/StudentList.java
@@ -0,0 +1,17 @@
+package module;
+
+
+import data.Data;
+
+// 수강생 목록 조회 - 혜민님
+public class StudentList {
+  Data data = new Data();
+  StudentList(){}
+  StudentList(Data data){
+    this.data = data;
+  }
+
+  void printStudentAll(){
+    //data.getStudents().
+  }
+}
