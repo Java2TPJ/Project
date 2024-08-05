eacc1aa8a4b25acc9b5bbdc72ccaabd2de10c851
Hyemin Yang <asitwas729@gmail.com>
2024-08-05 10:31:18 +0900

    메서드 별 구분하기 쉽게 기능과 담당(혜민)을 주석으로 적음

--- src/main/java/module/StudentList.java
+++ src/main/java/module/StudentList.java
@@ -7,7 +7,6 @@ import model.Student;
 import java.util.List;
 import java.util.Scanner;
 
-// 수강생 목록 조회 - 혜민님
 public class StudentList {
   Data data = new Data();
   Scanner sc = new Scanner(System.in);
@@ -17,6 +16,7 @@ public class StudentList {
     this.data = data;
   }
 
+  // (1-2) 수강생 목록 조회 - 혜민
   public void printStudentAll(){
     List<Student> students = data.getStudents();
     System.out.println("==================================");
@@ -35,8 +35,11 @@ public class StudentList {
         answerYes = false;
       }
     }
+  }
 
-
+  // (1-3) 상태별 수강생 목록 조회 - 혜민
+  public void stateStudentList(){
 
   }
+
 }
