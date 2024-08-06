94a54e4e334f14bbbb11fb3e7b7368c23fdcc0aa
taeyoung kang <kty1467@naver.com>
2024-08-06 18:06:22 +0900

    fix : View 클래스, 숫자가 아닌 문자를 입력받을 때 강종에 대한 대처 마련

--- src/main/java/view/StudentView.java
+++ src/main/java/view/StudentView.java
@@ -36,18 +36,23 @@ public class StudentView implements View{
             System.out.println("7. 메인 화면 이동");
             System.out.println("\n==================================");
             System.out.print("번호 입력 : ");
-            int i = sc.nextInt();
-
-            switch (i) {
-                case 1 -> {
-                    createStudent.studentSelect();
+            try {
+                int i = sc.nextInt();
+                sc.nextLine(); // 버퍼 비우기
+                switch (i) {
+                    case 1 -> {
+                        createStudent.studentSelect();
+                    }
+                    case 2 -> studentsList.printStudentAll();// 수강생 목록 조회
+                    case 3 -> studentInfo.showInfo(); // 수강생 정보 조회
+                    case 4 -> reNameStore.reName(); // 수강생 정보 수정
+                    case 5 -> deleteStudent.deleteStudent();// 수강생 삭제
+                    case 6 -> {studentsList.stateStudentList();} // 상태별 수강생 목록 조회
+                    case 7 -> flag = false; // 메인 화면 이동
                 }
-                case 2 -> studentsList.printStudentAll();// 수강생 목록 조회
-                case 3 -> studentInfo.showInfo(); // 수강생 정보 조회
-                case 4 -> reNameStore.reName(); // 수강생 정보 수정
-                case 5 -> deleteStudent.deleteStudent();// 수강생 삭제
-                case 6 -> {studentsList.stateStudentList();} // 상태별 수강생 목록 조회
-                case 7 -> flag = false; // 메인 화면 이동
+            } catch (InputMismatchException e) {
+                System.out.println("숫자만 입력해주세요.");
+                sc.nextLine(); // 버퍼 비우기
             }
         }
     }
